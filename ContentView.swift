2f93a77fee1d904c668be6a3be5023fa643e0a2a
Alberto Ortiz Zavaleta <aventoz@hotmail.com>
2026-05-12 22:42:57 -0600

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App-Cuiro
+//
+//  Created by Alberto Ortiz zavaleta on 10/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
